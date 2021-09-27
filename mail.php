8a4791e2512daa21b8b41299cc7f036916ad288c
Fobiya <stefanchuk.roman@gmail.com>
2021-09-27 14:05:22 +0300

    27.09.2021

--- mail.php
+++ mail.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -7,79 +7,52 @@ $c = true;
 if ( $method === 'POST' ) {
 
 	$project_name = trim($_POST["project_name"]);
-	$admin_email  = trim($_POST["admin_email"]);
+// 	$admin_email  = trim($_POST["admin_email"]);
 	$form_subject = trim($_POST["form_subject"]);
-	
+  
+	$admin_email  = 'YOUMAIL';
+
+
 	$NAME = $_POST['name'];
     $PHONE = $_POST['phone'];
     $email = $_POST['email'];
-    $messagelist = $_POST['messagelist'];
-  
-  
-    $zaezd= $_POST['zaezd'];
-    $vuezd = $_POST['vuezd'];
-  
-  
-    $lodka = $_POST['lodka'];
-    $prozhivaniye = $_POST['prozhivaniye'];
-    $pitaniye = $_POST['pitaniye'];
 
-  
-  
-    $uslugiokhotnika = $_POST['uslugiokhotnika'];
-    $arendamotora = $_POST['arendamotora'];
-    $mangal = $_POST['mangal'];
-    $koptilnya = $_POST['koptilnya'];
-    $zamorazhivaniye_ryby = $_POST['zamorazhivaniye_ryby'];
-    $transfer = $_POST['transfer'];
-    $message = $_POST['message'];
-
-	
-	
 
 	foreach ( $_POST as $key => $value ) {
-		
+
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" && $key != "start_day" && $key != "campaign_token" ) {
+          
+   
+          if ($key == "name") {$key = "Имя";} 
+          if ($key == "text") {$key = "Телефон";} 
+          if ($key == "priseauto") {$key = "Срок"; $value = $value.' Mес'; } 
+          if ($key == "month") {$key = "Стоимость "; $value = $value.' $';} 
+          if ($key == "advance") {$key = "Авансовый "; $value = $value.' %';} 
+          if ($key == "monthprice") {$key = "Расчет";} 
+          if ($key == "urlsite") {$key = "Ссылка";} 
+          
+          
 			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value </td>
 		</tr>
 		";
-		
+
 
 	}
 }
 } else if ( $method === 'GET' ) {
 
-	
+
 	$project_name = trim($_GET["project_name"]);
 	$admin_email  = trim($_GET["admin_email"]);
 	$form_subject = trim($_GET["form_subject"]);
-  
+
 	$NAME = $_POST['name'];
     $PHONE = $_POST['phone'];
     $email = $_POST['email'];
-    $messagelist = $_POST['messagelist'];
-  
-  
-    $zaezd= $_POST['zaezd'];
-    $vuezd = $_POST['vuezd'];
-  
-  
-    $lodka = $_POST['lodka'];
-    $prozhivaniye = $_POST['prozhivaniye'];
-    $pitaniye = $_POST['pitaniye'];
 
-  
-  
-    $uslugiokhotnika = $_POST['uslugiokhotnika'];
-    $arendamotora = $_POST['arendamotora'];
-    $mangal = $_POST['mangal'];
-    $koptilnya = $_POST['koptilnya'];
-    $zamorazhivaniye_ryby = $_POST['zamorazhivaniye_ryby'];
-    $transfer = $_POST['transfer'];
-    $message = $_POST['message'];
 
 
 	foreach ( $_GET as $key => $value ) {
@@ -90,7 +63,7 @@ if ( $method === 'POST' ) {
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
 		</tr>
 		";
-		
+
 	}
 }
 }
@@ -106,8 +79,8 @@ function adopt($text) {
 $headers = "MIME-Version: 1.0" . PHP_EOL .
 "Content-Type: text/html; charset=utf-8" . PHP_EOL .
 
-'From:  <log@voxlink.ru> '.adopt($project_name).'' . PHP_EOL .
-'Reply-To: log@voxlink.ru' . PHP_EOL;
+'From:  <mail@DOMEN> '.adopt($project_name).'' . PHP_EOL .
+'Reply-To: YOUMAIL' . PHP_EOL;
 
 
 
